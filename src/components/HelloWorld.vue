cc6aa5a7f4e21a4a3ba914bc7ae73f92b4e099ac
Emerson Fernandes <emerson_feral@hotmail.com>
2022-03-07 20:21:21 +0100

    Changing the Home component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,43 +1,32 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
+<script>
+
+export default {
+  data() {
+    return {
+      names: [
+        { id: 0, name: 'Emerson' },
+        { id: 1, name: 'Sphirot' },
+        { id: 2, name: 'Max' }
+      ]
+    };
   }
-});
+};
+
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+  <div>
+    <h1>Pokemon List</h1>
+    <ul>
+      <li
+      v-for="item in names" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
+     <RouterLink to="/second">second</RouterLink>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
