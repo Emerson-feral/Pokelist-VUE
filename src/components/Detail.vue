db32a26c984b775ad5ac6d7c1b9e834298e510d4
Emerson Fernandes <emerson_feral@hotmail.com>
2022-03-09 13:07:12 +0100

    Changing the components names

---
+++ src/components/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>S E C O N D</h1>
+    <router-link to="/">Pokelist</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
